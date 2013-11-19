51602ba98663c8390d634551f68578f21d104bd0
Zach Whaley <zachbwhaley@gmail.com>
2013-11-18 21:27:05 -0600

    simple list

--- android/src/com/studly/MainActivity.java
+++ android/src/com/studly/MainActivity.java
@@ -1,22 +1,30 @@
 package com.studly;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	public static final String[] THINGS = {
+        "Android",
+        "iPhone",
+        "WindowsMobile",
+        "Blackberry",
+        "WebOS",
+        "Ubuntu",
+        "Windows",
+        "Max OS X",
+        "Linux",
+        "OS/2"
+	};
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, THINGS);
+		setListAdapter(adapter);
+	}
 
 }
