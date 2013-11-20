6bdd3a72f86a3dc0560b5be7ca8ce89cdde56f8f
Zach Whaley <zachbwhaley@gmail.com>
2013-11-19 21:04:57 -0600

    Added Join click listener

--- android/src/com/studly/activity/MainActivity.java
+++ android/src/com/studly/activity/MainActivity.java
@@ -1,28 +1,103 @@
 package com.studly.activity;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.studly.adapter.StudlyAdapter;
+import com.octo.android.robospice.SpiceManager;
+import com.studly.R;
+import com.studly.model.StudlyEvent;
 import com.studly.network.RequestStudlyEvents;
+import com.studly.service.StudlyService;
+import com.studly.util.AccountUtils;
 
 public class MainActivity extends ListActivity {
 
     private static final String TAG = "MainActivity";
 
+    /* Attributes */
+    
+    private SpiceManager mSpiceManager = new SpiceManager(StudlyService.class);
     private RequestStudlyEvents mRequestStudlyEvents;
 
+    /* Internal Classes */
+    
+    private class StudlyAdapter extends ArrayAdapter<StudlyEvent> {
+        public StudlyAdapter(Context context, StudlyEvent.List events) {
+            super(context, 0, events);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.d(TAG, "Create new view");
+            LayoutInflater inflater = getLayoutInflater();
+            View view = inflater.inflate(R.layout.studly_list_item, null);
+
+            StudlyEvent event = getItem(position);
+            if (event != null) {
+                Log.d(TAG, "creating row for event " + event.getName());
+                
+                TextView textView = (TextView) view.findViewById(R.id.row_event);
+                textView.setText(event.getName());
+                
+                Button join = (Button) view.findViewById(R.id.button_join);
+                join.setOnClickListener(new JoinClickListener(event));
+            }
+            return view;
+        }
+    }
+    
+    private class JoinClickListener implements OnClickListener {
+        private StudlyEvent event;
+        public JoinClickListener(StudlyEvent event) {
+            this.event = event;
+        }
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Join " + event.getName(), Toast.LENGTH_SHORT).show();
+            final String account = AccountUtils.getChosenAccountName(MainActivity.this);
+            if (TextUtils.isEmpty(account)) {
+                // TODO set chosen account
+            }
+            //mSpiceManager.execute(request, requestListener);
+        }
+    }
+    
+    /* Methods */
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mRequestStudlyEvents = new RequestStudlyEvents();
         try {
             Log.d(TAG, "Setting list adapter");
+            //mSpiceManager.execute(mRequestStudlyEvents, requestListener);
             setListAdapter(new StudlyAdapter(this, mRequestStudlyEvents.loadDataFromNetwork()));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mSpiceManager.start(this);
+    }
+
+    @Override
+    protected void onStop() {
+        mSpiceManager.shouldStop();
+        super.onStop();
+    }
+    
 }
---
+++ android/src/com/studly/util/AccountUtils.java
@@ -0,0 +1,37 @@
+package com.studly.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public final class AccountUtils {
+    
+    private static final String TAG = "AccountUtils";
+    private static final String PREF_CHOSEN_ACCOUNT = "chosen_account";
+    private static final String GOOGLE_ACCOUNT_TYPE = "com.google";
+    
+    private AccountUtils() {
+    }
+    
+    public static List<Account> getAccounts(final Context context) {
+        AccountManager accountManager = AccountManager.get(context);
+        return Arrays.asList(accountManager.getAccountsByType(GOOGLE_ACCOUNT_TYPE));
+    }
+
+    public static String getChosenAccountName(final Context context) {
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        return sp.getString(PREF_CHOSEN_ACCOUNT, null);
+    }
+    
+    static void setChosenAccountName(final Context context, final String accountName) {
+        Log.d(TAG, "Chose account " + accountName);
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(context);
+        sp.edit().putString(PREF_CHOSEN_ACCOUNT, accountName).commit();
+    }
+}
