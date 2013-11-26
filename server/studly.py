8f586f18fd8487b82c6e8e4e285952aaccb0571d
trevorlatson <trevor.latson@gmail.com>
2013-11-26 13:22:18 -0600

    added library dependencies for calendarListUpdate.py; all imports now work except for mySQLconnect.py

--- server/studly.py
+++ server/studly.py
@@ -1,5 +1,6 @@
 import json
 import webapp2
+import calendarListUpdate
 
 from google.appengine.ext import ndb
 
@@ -16,7 +17,20 @@ class GetGroups(webapp2.RequestHandler):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.out.write(json.dumps(events))
 
+
+class GetReflectorList(webapp2.RequestHandler):
+    def get(self):          
+        with open('view/reflectors.html', 'r') as f:
+            html = f.read()
+        self.response.out.write(html)
+         
+    def post(self):
+        fileName = self.request.get('file')  
+        self.response.out.write(fileName)
+                
+
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/groups', GetGroups),
+    ('/reflectors', GetReflectorList),
 ], debug=True)
