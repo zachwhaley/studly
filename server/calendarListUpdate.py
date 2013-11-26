5e3191ac7206a3bed55891564a11bd1a77b714ef
trevorlatson <trevor.latson@gmail.com>
2013-11-26 15:03:08 -0600

    added wrappers for all calendarListUpdate.py functions

--- server/calendarListUpdate.py
+++ server/calendarListUpdate.py
@@ -9,20 +9,21 @@ import calendarAuthenticate
 from dateutil import parser
 
 def getReflectorList(reflectorList, displayWarning = False):
-    reflectorList = "/var/lib/majordomo/lists/" + reflectorList
-    try: 
-        refList = open(reflectorList , 'r' )
-    except:
-      if displayWarning == True:
-        print "WARNING: Reflector List not found: " + reflectorList
-        exit(1)
-    emails = []
-    while True:
-        oneLine = refList.readline()
-        if not oneLine:
-            break
-        if len(oneLine) > 4:
-            emails.append(str.strip(oneLine.lower()))
+#     reflectorList = "/var/lib/majordomo/lists/" + reflectorList
+#     try: 
+#         refList = open(reflectorList , 'r' )
+#     except:
+#       if displayWarning == True:
+#         print "WARNING: Reflector List not found: " + reflectorList
+#         exit(1)
+#     emails = []
+#     while True:
+#         oneLine = refList.readline()
+#         if not oneLine:
+#             break
+#         if len(oneLine) > 4:
+#             emails.append(str.strip(oneLine.lower()))
+    emails = ["trevor.latson@gmail.com", "zachbwhaley@gmail.com"]
     return emails
 
 def getCalendarList(calendarId):
--- server/studly.py
+++ server/studly.py
@@ -20,17 +20,50 @@ class GetGroups(webapp2.RequestHandler):
 
 class GetReflectorList(webapp2.RequestHandler):
     def get(self):          
-        with open('view/reflectors.html', 'r') as f:
-            html = f.read()
-        self.response.out.write(html)
+        reflectorList = self.request.get('reflectorList')
+        emails = calendarListUpdate.getReflectorList(reflectorList, displayWarning = False) 
+        self.response.out.write(emails)
+        
+class GetCalendarList(webapp2.RequestHandler):
+    def get(self):          
+        calendarId = self.request.get('calendarId')
+        calendar_list = calendarListUpdate.getReflectorList(calendarId) 
+        self.response.out.write(calendar_list)
+        
+class GetEvents(webapp2.RequestHandler):
+    def get(self):          
+        calendarId = self.request.get('calendarId')
+        events = calendarListUpdate.getEvents(calendarId, pageToken=None) 
+        self.response.out.write(events)
+        
+class GetSingleEvents(webapp2.RequestHandler):
+    def get(self):          
+        calendarId = self.request.get('calendarId')
+        events = calendarListUpdate.getSingleEvents(calendarId, pageToken=None) 
+        self.response.out.write(events)
          
-    def post(self):
-        fileName = self.request.get('file')  
-        self.response.out.write(fileName)
-                
+class UpdateEvent(webapp2.RequestHandler):
+    def get(self):
+        event = self.request.get('event')          
+        calendarId = self.request.get('calendarId')
+        reflectorList = self.request.get('reflectorList')
+        calendarListUpdate.updateEvent(event, calendarId, reflectorList)
+        
+class UpdateCalendarList(webapp2.RequestHandler):
+    def get(self):
+        calendarId = self.request.get('calendarId')
+        TimezoneOffset = self.request.get('TimezoneOffset')
+        calendarListUpdate.updateCalendarList(calendarId, TimezoneOffset, debug = True)
+        
 
+                
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/groups', GetGroups),
     ('/reflectors', GetReflectorList),
+    ('/calendars', GetCalendarList),
+    ('/events', GetEvents),
+    ('/single-events', GetSingleEvents),
+    ('/update-event', UpdateEvent),
+    ('/update-calendar', UpdateCalendarList)
 ], debug=True)
