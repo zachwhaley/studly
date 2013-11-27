054679dca6f749ced8796317ba3c9b316d54f024
Zach Whaley <zachbwhaley@gmail.com>
2013-11-26 18:20:05 -0600

    added mock calls

--- server/studly.py
+++ server/studly.py
@@ -7,16 +7,42 @@ class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.out.write("Studly")
 
-class GetGroups(webapp2.RequestHandler):
-    def get(self):
-        events = [
-            {"name": "foo", "joined": True},
-            {"name": "bar", "joined": False}
-        ]
+class GetReflectorList(webapp2.RequestHandler):
+    def get(self):          
         self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write(json.dumps(events))
+        self.response.out.write(json.dumps(["foo", "bar"]))
+        
+class GetCalendarList(webapp2.RequestHandler):
+    def get(self):          
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(["foo", "bar"]))
+        
+class GetEvents(webapp2.RequestHandler):
+    def get(self):          
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(["foo", "bar"]))
+        
+class GetSingleEvents(webapp2.RequestHandler):
+    def get(self):          
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(["foo", "bar"]))
+         
+class UpdateEvent(webapp2.RequestHandler):
+    def post(self):
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(True))
+        
+class UpdateCalendarList(webapp2.RequestHandler):
+    def post(self):
+        self.response.headers['Content-Type'] = 'application/json'
+        self.response.out.write(json.dumps(True))
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/groups', GetGroups),
+    ('/reflectors.json', GetReflectorList),
+    ('/calendars.json', GetCalendarList),
+    ('/events.json', GetEvents),
+    ('/single-events.json', GetSingleEvents),
+    ('/update-event', UpdateEvent),
+    ('/update-calendar', UpdateCalendarList)
 ], debug=True)
