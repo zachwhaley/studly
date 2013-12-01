bccb4fdbaba7fb30abc566dc5b2cfab65f60e7a1
Zach Whaley <zachbwhaley@gmail.com>
2013-12-01 09:33:36 -0600

    change of server code

--- server/studly.py
+++ server/studly.py
@@ -1,304 +1,32 @@
 import json
 import webapp2
 
-from google.appengine.ext import ndb
-
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.out.write("Studly")
 
-class GetReflectorList(webapp2.RequestHandler):
+class GetMappings(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write(json.dumps(
-            {"name": "refs", "list": ["emailaddress1@foo.com", "emailaddress2@foo.com"]}
-        ))
+        mappings = [{"title": "testEvent1",
+                     "reflectorList": ["trevor.latson@gmail.com", "zachbwhaley@gmail.com"],
+                     "nextStartTime": "now!",
+                     "recurringTime": "Every Tuesday at 10:00am",
+                     "location": "Trevor's House"},
+                    {"title": "testEvent1",
+                     "reflectorList": ["trevor.latson@gmail.com", "zachbwhaley@gmail.com"],
+                     "nextStartTime": "now!",
+                     "recurringTime": "Every Tuesday at 10:00am",
+                     "location": "Trevor's House"},
+        ]
+        self.response.out.write(json.dumps(mappings))
 
-class GetCalendarList(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'application/json'
-        calendar_list = [{
-                          "kind": "calendar#calendarListEntry",
-                          "etag": "0MAR7UdiMyUNDlceNGt_2sHPXk4/aOZ-T3e4J4bEfydljdeBBOVF8UU",
-                          "id": "trevor.latson@gmail.com",
-                          "summary": "trevor.latson@gmail.com",
-                          "description": "Your contacts' birthdays and anniversaries",
-                          "location": "Austin",
-                          "timeZone": "America/Chicago",
-                          "summaryOverride": "I've never seen this used...",
-                          "colorId": "22",
-                          "backgroundColor": "#fa573c",
-                          "foregroundColor": "#000000",
-                          "hidden": False,
-                          "selected": True,
-                          "accessRole": "reader",
-                          "defaultReminders": [
-                            {
-                              "method": "email",
-                              "minutes": 30
-                            }
-                          ],
-                          "primary": True
-                          },
-                         {
-                          "kind": "calendar#calendarListEntry",
-                          "etag": "etag string",
-                          "id": "string",
-                          "summary": "string",
-                          "description": "string",
-                          "location": "string",
-                          "timeZone": "string",
-                          "summaryOverride": "string",
-                          "colorId": "string",
-                          "backgroundColor": "string",
-                          "foregroundColor": "string",
-                          "hidden": True,
-                          "selected": False,
-                          "accessRole": "string",
-                          "defaultReminders": [
-                            {
-                              "method": "string",
-                              "minutes": 5
-                            }
-                          ],
-                          "primary": False
-}]
-        self.response.out.write(json.dumps(calendar_list))
-
-class GetEvents(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'application/json'
-        events = [{
-                  "kind": "calendar#event",
-                  "etag": "etag",
-                  "id": "string",
-                  "status": "string",
-                  "htmlLink": "string",
-                  "created": "today",
-                  "updated": "today",
-                  "summary": "string",
-                  "description": "string",
-                  "location": "string",
-                  "colorId": "string",
-                  "creator": {
-                    "id": "string",
-                    "email": "string",
-                    "displayName": "string",
-                    "self": True
-                  },
-                  "organizer": {
-                    "id": "string",
-                    "email": "string",
-                    "displayName": "string",
-                    "self": True
-                  },
-                  "start": {
-                    "date": "today",
-                    "dateTime": "today",
-                    "timeZone": "string"
-                  },
-                  "end": {
-                    "date": "today",
-                    "dateTime": "today",
-                    "timeZone": "string"
-                  },
-                  "endTimeUnspecified": True,
-                  "recurrence": [
-                    "string"
-                  ],
-                  "recurringEventId": "string",
-                  "originalStartTime": {
-                    "date": "today",
-                    "dateTime": "today",
-                    "timeZone": "string"
-                  },
-                  "transparency": "string",
-                  "visibility": "string",
-                  "iCalUID": "string",
-                  "sequence": 2,
-                  "attendees": [
-                    {
-                      "id": "string",
-                      "email": "string",
-                      "displayName": "string",
-                      "organizer": True,
-                      "self": True,
-                      "resource": True,
-                      "optional": True,
-                      "responseStatus": "string",
-                      "comment": "string",
-                      "additionalGuests": 2
-                    }
-                  ],
-                  "attendeesOmitted": True,
-                  "extendedProperties": {
-                    "private": {
-                      "(key)": "string"
-                    },
-                    "shared": {
-                      "(key)": "string"
-                    }
-                  },
-                  "hangoutLink": "string",
-                  "gadget": {
-                    "type": "string",
-                    "title": "string",
-                    "link": "string",
-                    "iconLink": "string",
-                    "width": 2,
-                    "height": 2,
-                    "display": "string",
-                    "preferences": {
-                      "(key)": "string"
-                    }
-                  },
-                  "anyoneCanAddSelf": True,
-                  "guestsCanInviteOthers": True,
-                  "guestsCanModify": True,
-                  "guestsCanSeeOtherGuests": True,
-                  "privateCopy": True,
-                  "locked": True,
-                  "reminders": {
-                    "useDefault": True,
-                    "overrides": [
-                      {
-                        "method": "string",
-                        "minutes": 2
-                      }
-                    ]
-                  },
-                  "source": {
-                    "url": "string",
-                    "title": "string"
-                  }
-                }]
-        self.response.out.write(json.dumps(events))
-
-class GetSingleEvent(webapp2.RequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'application/json'
-        events = [{
-                  "kind": "calendar#event",
-                  "etag": "etag",
-                  "id": "string",
-                  "status": "string",
-                  "htmlLink": "string",
-                  "created": "today",
-                  "updated": "today",
-                  "summary": "string",
-                  "description": "string",
-                  "location": "string",
-                  "colorId": "string",
-                  "creator": {
-                    "id": "string",
-                    "email": "string",
-                    "displayName": "string",
-                    "self": True
-                  },
-                  "organizer": {
-                    "id": "string",
-                    "email": "string",
-                    "displayName": "string",
-                    "self": True
-                  },
-                  "start": {
-                    "date": "today",
-                    "dateTime": "today",
-                    "timeZone": "string"
-                  },
-                  "end": {
-                    "date": "today",
-                    "dateTime": "today",
-                    "timeZone": "string"
-                  },
-                  "endTimeUnspecified": True,
-                  "recurrence": [
-                    "string"
-                  ],
-                  "recurringEventId": "string",
-                  "originalStartTime": {
-                    "date": "today",
-                    "dateTime": "today",
-                    "timeZone": "string"
-                  },
-                  "transparency": "string",
-                  "visibility": "string",
-                  "iCalUID": "string",
-                  "sequence": 2,
-                  "attendees": [
-                    {
-                      "id": "string",
-                      "email": "string",
-                      "displayName": "string",
-                      "organizer": True,
-                      "self": True,
-                      "resource": True,
-                      "optional": True,
-                      "responseStatus": "string",
-                      "comment": "string",
-                      "additionalGuests": 2
-                    }
-                  ],
-                  "attendeesOmitted": True,
-                  "extendedProperties": {
-                    "private": {
-                      "(key)": "string"
-                    },
-                    "shared": {
-                      "(key)": "string"
-                    }
-                  },
-                  "hangoutLink": "string",
-                  "gadget": {
-                    "type": "string",
-                    "title": "string",
-                    "link": "string",
-                    "iconLink": "string",
-                    "width": 2,
-                    "height": 2,
-                    "display": "string",
-                    "preferences": {
-                      "(key)": "string"
-                    }
-                  },
-                  "anyoneCanAddSelf": True,
-                  "guestsCanInviteOthers": True,
-                  "guestsCanModify": True,
-                  "guestsCanSeeOtherGuests": True,
-                  "privateCopy": True,
-                  "locked": True,
-                  "reminders": {
-                    "useDefault": True,
-                    "overrides": [
-                      {
-                        "method": "string",
-                        "minutes": 2
-                      }
-                    ]
-                  },
-                  "source": {
-                    "url": "string",
-                    "title": "string"
-                  }
-                }]
-        self.response.out.write(json.dumps(events))
-
-class UpdateEvent(webapp2.RequestHandler):
+class EventManager(webapp2.RequestHandler):
     def post(self):
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write(json.dumps(True))
-
-class UpdateCalendarList(webapp2.RequestHandler):
-    def post(self):
-        self.response.headers['Content-Type'] = 'application/json'
-        self.response.out.write(json.dumps(True))
+        self.redirect('/groups')
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/reflectors.json', GetReflectorList),
-    ('/calendars.json', GetCalendarList),
-    ('/events.json', GetEvents),
-    ('/single-event.json', GetSingleEvent),
-    ('/update-event', UpdateEvent),
-    ('/update-calendar', UpdateCalendarList)
+    ('/mappings.json', GetMappings),
+    ('/join', EventManager),
+    ('/leave', EventManager),
 ], debug=True)
