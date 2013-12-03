12c6abdb041313d75a4dbf55a8a2581a99895c1e
Zach Whaley <zachbwhaley@gmail.com>
2013-12-02 20:59:43 -0600

    cleanup

--- server/studly.py
+++ server/studly.py
@@ -8,18 +8,14 @@ from apiclient.discovery import build
 from google.appengine.ext import webapp
 from oauth2client.appengine import OAuth2DecoratorFromClientSecrets
 
-# decorator = OAuth2Decorator(
-#   client_id='656176414432-sdq1gmm8csamg9m1ac0u0482gknbhjvh.apps.googleusercontent.com',
-#   client_secret='nN6SnG9tZN7LaKQIYZW2J4bA',
-#   scope='https://www.googleapis.com/auth/calendar')
-
 decorator = OAuth2DecoratorFromClientSecrets(
   os.path.join(os.path.dirname(__file__), 'client_secret_656176414432.apps.googleusercontent.com.json'),
   scope='https://www.googleapis.com/auth/calendar')
 
 service = build('calendar', 'v3')
 
-class Mappings(ndb.Model):
+
+class Mapping(ndb.Model):
     title = ndb.StringProperty()
     htmlLink = ndb.StringProperty()
     reflectorList = ndb.StringProperty(repeated=True)
@@ -28,6 +24,7 @@ class Mappings(ndb.Model):
     recurringStartTime = ndb.StringProperty()
     location = ndb.StringProperty()
 
+
 class AddEmail(webapp2.RequestHandler):
     def get(self):
         self.redirect('/myevents')
@@ -35,9 +32,9 @@ class AddEmail(webapp2.RequestHandler):
     def post(self):
         # Get the authorized Http object created by the decorator.
         http = decorator.http()
-        
+
         # Retrieve the mapping with the title specified
-        mappings = Mappings.query(Mappings.title == self.request.get('title')).fetch()
+        mappings = Mapping.query(Mapping.title == self.request.get('title')).fetch()
         # Iterate through each mapping object that matches the specified event title (usually just one)
         for mapping in mappings:
             # Append the specified emailAddress to the mapping object
@@ -47,6 +44,7 @@ class AddEmail(webapp2.RequestHandler):
             [mp.put() for mp in calendarListUpdate.updateCalendarList([mapping], self.request.get('calendarId'), http)]
         self.redirect('/myevents')
 
+
 class RemoveEmail(webapp2.RequestHandler):
     def get(self):
         self.redirect('/myevents')
@@ -54,9 +52,9 @@ class RemoveEmail(webapp2.RequestHandler):
     def post(self):
         # Get the authorized Http object created by the decorator.
         http = decorator.http()
-        
+
         # Retrieve the mapping with the title specified
-        mappings = Mappings.query(Mappings.title == self.request.get('title')).fetch()
+        mappings = Mapping.query(Mapping.title == self.request.get('title')).fetch()
         # Iterate through each mapping object that matches the specified event title (usually just one)
         for mapping in mappings:
             # Append the specified emailAddress to the mapping object
@@ -65,99 +63,26 @@ class RemoveEmail(webapp2.RequestHandler):
             # And store the updated mapping object in the datastore
             [mp.put() for mp in calendarListUpdate.updateCalendarList([mapping], self.request.get('calendarId'), http)]
         self.redirect('/myevents')
-    
+
+
 class GetMappings(webapp2.RequestHandler):
     def get(self):
         # Sweet one-liner.
-        self.response.out.write(json.dumps([m.to_dict() for m in Mappings.query().fetch()]))
-    
-class SetMappings(webapp2.RequestHandler):
-    def get(self):
-        with open('set-mappings.html', 'r') as f:
-            html = f.read()
-        self.response.out.write(html) 
-        
-    def post(self):
-        mapping = Mappings()
-        mapping.title = self.request.get('title')
-        reflectorsString = self.request.get('reflectorList')
-        # Parse the string into a list of email addresses and store it into the reflectorList field
-        reflectors = reflectorsString.split(',')
-        for reflector in reflectors:
-            mapping.reflectorList.append(reflector.lower())
-        mapping.calendarId = self.request.get('calendarId')
-        mapping.nextStartTime = self.request.get('nextStartTime')
-        mapping.recurringStartTime = self.request.get('recurringStartTime')
-        mapping.location = self.request.get('location')
-        mapping.put()
+        self.response.out.write(json.dumps([m.to_dict() for m in Mapping.query().fetch()]))
 
-        
+
 class GetCalendarList(webapp2.RequestHandler):
     @decorator.oauth_required
-    def get(self):          
-        # Get the authorized Http object created by the decorator.
-        http = decorator.http()
-        
-        #hard-coded data for testing purposes:
-        calendarId = "trevor.latson@gmail.com"
-        
-        calendar_list = calendarListUpdate.getCalendarList(calendarId, http) 
-        self.response.out.write(json.dumps(calendar_list))
-        
-class GetSettings(webapp2.RequestHandler):
-    @decorator.oauth_required
-    def get(self):          
+    def get(self):
         # Get the authorized Http object created by the decorator.
         http = decorator.http()
 
-        settings = calendarListUpdate.getSettings(http) 
-        self.response.out.write(json.dumps(settings))
-        
-        
-class GetEvents(webapp2.RequestHandler):
-    @decorator.oauth_required
-    def get(self):          
-        # Get the authorized Http object created by the decorator.
-        http = decorator.http()
-        
         #hard-coded data for testing purposes:
         calendarId = "trevor.latson@gmail.com"
-        
-        events = calendarListUpdate.getEvents(calendarId, http) 
-        self.response.out.write(json.dumps(events))
-        
-        
-class GetSingleEvents(webapp2.RequestHandler):
-    @decorator.oauth_required
-    def get(self):          
-        # Get the authorized Http object created by the decorator.
-        http = decorator.http()
-        
-        #hard-coded data for testing purposes:
-        calendarId = "trevor.latson@gmail.com"
-        
-        singleEvents = calendarListUpdate.getSingleEvents(calendarId, http) 
-        self.response.out.write(json.dumps(singleEvents))
-         
-        
-class UpdateCalendarList(webapp2.RequestHandler):
-    @decorator.oauth_required
-    def get(self):
-        with open('update-calendar.html', 'r') as f:
-            html = f.read()
-        self.response.out.write(html)
-        
-    @decorator.oauth_required
-    def post(self):
-        # Get the authorized Http object created by the decorator.
-        http = decorator.http()
-        
-        calendarId = self.request.get('calendarId')
-        mappings = [m.to_dict() for m in Mappings.query(Mappings.calendarId == calendarId).fetch()]
-        
-        
-        response = calendarListUpdate.updateCalendarList(mappings, calendarId, http)
-        self.response.out.write(response)
+
+        calendar_list = calendarListUpdate.getCalendarList(calendarId, http)
+        self.response.out.write(json.dumps(calendar_list))
+
 
 class ImportEvent(webapp2.RequestHandler):
     def get(self):
@@ -166,11 +91,12 @@ class ImportEvent(webapp2.RequestHandler):
     def post(self):
         # Get the authorized Http object created by the decorator.
         http = decorator.http()
-        mapping = Mappings()
+        mapping = Mapping()
         mapping.title = self.request.get('title')
         mapping.calendarId = self.request.get('calendarId')
         [mp.put() for mp in calendarListUpdate.updateCalendarList([mapping], mapping.calendarId, http)]
         self.redirect('/myevents')
+
 
 class JoinEvent(webapp2.RequestHandler):
     def get(self):
@@ -179,9 +105,9 @@ class JoinEvent(webapp2.RequestHandler):
     def post(self):
         # Get the authorized Http object created by the decorator.
         http = decorator.http()
-        
+
         # Retrieve the mapping with the title specified
-        mappings = Mappings.query(Mappings.title == self.request.get('title')).fetch()
+        mappings = Mapping.query(Mapping.title == self.request.get('title')).fetch()
         # Iterate through each mapping object that matches the specified event title (usually just one)
         for mapping in mappings:
             # Append the specified emailAddress to the mapping object
@@ -191,28 +117,24 @@ class JoinEvent(webapp2.RequestHandler):
             [mp.put() for mp in calendarListUpdate.updateCalendarList([mapping], self.request.get('calendarId'), http)]
         self.redirect('/')
 
+
 class RemoveEvent(webapp2.RequestHandler):
     def get(self):
         self.redirect('/myevents')
     def post(self):
         # Retrieve the mapping with the title specified
-        mappings = Mappings.query(Mappings.title == self.request.get('title')).fetch()
+        mappings = Mapping.query(Mapping.title == self.request.get('title')).fetch()
         # Iterate through each mapping object that matches the specified event title (usually just one)
         for mapping in mappings:
             # Remove the mapping from the datastore
             mapping.key.delete()
         self.redirect('/myevents')
-    
+
+
 app = webapp2.WSGIApplication([
     ('/get-mappings.json', GetMappings),
-    ('/set-mappings.json', SetMappings),
-    ('/calendars.json', GetCalendarList),
-    ('/get-settings.json', GetSettings),
-    ('/events.json', GetEvents),
-    ('/single-events.json', GetSingleEvents),
     ('/add-email', AddEmail),
     ('/remove-email', RemoveEmail),
-    ('/update-calendar', UpdateCalendarList),
     ('/import-event', ImportEvent),
     ('/join-event', JoinEvent),
     ('/remove-event', RemoveEvent),
